7e299b7b967d1fb6750b27b61fc04e5ab2a324cd
akkdio <akkdio@gmail.com>
2013-07-21 16:08:49 -0400

    adding content_for files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContentFor::Application.config.secret_token = 'd7c146232639e692e223231bf0f7f0292736de24c84d715664b8da7c47ce6ca436e73d0a525b6b937858802cd219c0811340a27943df91ad58accb843202d210'
